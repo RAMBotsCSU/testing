31f0b9d6ce29560912cf7d570cd339620704d146
qwertyuioplark2 <113788238+qwertyuioplark2@users.noreply.github.com>
2022-10-24 18:43:18 -0600

    Update serial_test.ino

--- teensy/serial_test.ino
+++ teensy/serial_test.ino
@@ -1,9 +1,19 @@
+//********************************************************
+//* This is a simple test for the teensy that recieves   *
+//* a string over the serial line, appends some values   *
+//* then sends it back over serial. This method includes *
+//* padding functions for the serial communication to    *
+//* speed up the communication.                          *
+//********************************************************
+
+//Setup and set the pin for the LED
 int led = 13;
 void setup() {
   pinMode(led, OUTPUT);
   Serial.begin(9600);
 }
 
+//Function to pad the string to 120 chars (serial uses 127, but the write command adds 7 chars)
 String padStr(String str){
   int target = 120-str.length();
   for (int i = 0; i < target; i++) {
@@ -12,28 +22,27 @@ String padStr(String str){
   return str;
 }
 
+//Function to remove the padding of strings
 String rmPadStr(String str){
   for (int i = 0; i < str.length(); i++){
     if (str[i] == '~'){
-//      Serial.println("Found padding starting at index: " + i);
       str.remove(i);
       return str;
     }
   }
 }
 
+//Main loop to be executed
 void loop() {
-  //Serial.println("Enter data:");
-  //while (Serial.available() == 0) {}     //wait for data available
-  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
-  String readStr = Serial.readString();  //read until timeout
-  readStr.trim();                        // remove any \r \n whitespace at the end of the String
-  //readStr = rmPadStr(readStr);
-  if (!(readStr == "")){
-    digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
-    readStr = ("Button Pressed. I got: " + readStr);
-    //readStr = padStr(readStr);
-    Serial.println(readStr);
+  digitalWrite(led, LOW);                             //Set LED to off when no message has been recieved
+  //while (Serial.available() == 0) {}                //optional wait for serial input
+  String readStr = Serial.readString();               //Read the serial line and set to readStr
+  readStr.trim();                                     //Remove any \r \n whitespace at the end of the String
+  readStr = rmPadStr(readStr);                        //Remove any padding from readStr
+  if (readStr != ""){                                 //readStr == "" if the serial read had nothing in it
+    digitalWrite(led, HIGH);                          //Turn on the LED to show that a string with != "" has been recieved
+    readStr = ("Button Pressed. I got: " + readStr);  //Append a message to the string
+    readStr = padStr(readStr);                        //Add padding to the string
+    Serial.println(readStr);                          //Print to the serial buffer
   }
-//  testPadding();
 }
