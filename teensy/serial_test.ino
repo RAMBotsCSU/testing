5551b665722040c585c82371c72795a02098f15b
qwertyuioplark2 <113788238+qwertyuioplark2@users.noreply.github.com>
2022-11-02 17:01:09 -0600

    Add files via upload

--- teensy/serial_test.ino
+++ teensy/serial_test.ino
@@ -8,9 +8,35 @@
 
 //Setup and set the pin for the LED
 int led = 13;
+int setInd = -1;
+float setVal = -1.1;
+float movementArr[7]; //0 = strafe, 1 = forback, 2 = roll, 3 = turn, 4 = pitch, 5 = height, 6 = yaw
+
 void setup() {
   pinMode(led, OUTPUT);
   Serial.begin(9600);
+}
+
+String getArrStr(){
+  String stringFinal;
+  String String1 = "Teensy: strafe:";
+  String String2 = ", forback:";
+  String String3 = ", roll:";
+  String String4 = ", turn:";
+  String String5 = ", pitch:";
+  String String6 = ", height:";
+  String String7 = ", yaw:";
+  double Val1 = movementArr[0];
+  double Val2 = movementArr[1];
+  double Val3 = movementArr[2];
+  double Val4 = movementArr[3];
+  double Val5 = movementArr[4];
+  double Val6 = movementArr[5];
+  double Val7 = movementArr[6];
+
+  stringFinal = String1 + Val1 + String2 + Val2 + String3 + Val3 + String4 + Val4 + String5 + Val5 + String6 + Val6 + String7 + Val7;
+  return stringFinal;
+  
 }
 
 //Function to pad the string to 120 chars (serial uses 127, but the write command adds 7 chars)
@@ -42,8 +68,10 @@ void loop() {
   readStr = rmPadStr(readStr);                        //Remove any padding from readStr
   if (readStr != ""){                                 //readStr == "" if the serial read had nothing in it
     digitalWrite(led, HIGH);                          //Turn on the LED to show that a string with != "" has been recieved
-    readStr = ("Button Pressed. I got: " + readStr);  //Append a message to the string
-    readStr = padStr(readStr);                        //Add padding to the string
-    Serial.println(readStr);                          //Print to the serial buffer
+    setInd = (readStr.substring(readStr.indexOf("r")+1,readStr.indexOf(":"))).toInt();
+    setVal = (readStr.substring(readStr.indexOf(":")+1,readStr.indexOf(","))).toFloat();
+    movementArr[setInd] = setVal;
+    Serial.println(getArrStr());      //Print to the serial buffer
+    //Serial.println(setVal);
   }
 }
