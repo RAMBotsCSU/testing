e5ecf13674fcee9803bd761f5d201c2ff1e80496
percinet <percinet@colostate.edu>
2023-01-17 12:10:40 -0700

    speaker implementaiton

--- pi/PiGUI/pygame_Test.py
+++ pi/PiGUI/pygame_Test.py
@@ -1,14 +1,17 @@
 import pygame
 from pygame import mixer
 import time
+import random
 
 mixer.init()
 # apt-get install git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0
 
+sheep1 = pygame.mixer.Sound('Sounds/sheep1.mp3')
+sheep2 = pygame.mixer.Sound('Sounds/sheep2.mp3')
+sheep3 = pygame.mixer.Sound('Sounds/sheep3.mp3')
+sheep4 = pygame.mixer.Sound('Sounds/sheep4.mp3')
 
-mixer.music.load("Resources/beep.mp3")
-mixer.music.set_volume(0.7)
-
+sheep_sounds = [sheep1,sheep2,sheep3,sheep4]
 
 def imshow(filename):
     img = pygame.image.load(filename)
@@ -21,12 +24,12 @@ def imshow(filename):
 def imclose():
     pygame.display.quit()
 
-mixer.music.play()
+pygame.mixer.Sound.play(random.choice(sheep_sounds))
 
-imshow('Resources/RamBOTs_Logo.bmp')
-time.sleep(3)
-imclose()
+#imshow('Resources/RamBOTs_Logo.bmp')
+#time.sleep(3)
+#imclose()
 
-imshow('Resources/RamBOTs_Logo.bmp')
-time.sleep(3)
-imclose()
+#imshow('Resources/RamBOTs_Logo.bmp')
+#time.sleep(3)
+#imclose()
