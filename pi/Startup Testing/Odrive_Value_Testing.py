55678f4f95f696378497f3f9aca1ee0624b63b53
ersmith01 <elaines@colostate.edu>
2024-02-15 17:19:16 -0700

    Add odrive_values_test functionality and shells for the three helper functions

--- pi/Startup Testing/Odrive_Value_Testing.py
+++ pi/Startup Testing/Odrive_Value_Testing.py
@@ -35,68 +35,22 @@ correct_hip_values = {
                       }
 
 
-correct_shoulder_values = {
-                      "PARAM_FLOAT_POS_SETPOINT": 0,
-                      "PARAM_FLOAT_POS_GAIN": 0,
-                      "PARAM_FLOAT_VEL_SETPOINT": 0,
-                      "PARAM_FLOAT_VEL_GAIN": 0,
-                      "PARAM_FLOAT_VEL_INTEGRATOR_GAIN": 0,
-                      "PARAM_FLOAT_VEL_INTEGRATOR_CURRENT": 0,
-                      "PARAM_FLOAT_VEL_LIMIT": 0,
-                      "PARAM_FLOAT_CURRENT_SETPOINT": 0,
-                      "PARAM_FLOAT_CALIBRATION_CURRENT": 0,
-                      "PARAM_FLOAT_PHASE_INDUCTANCE": 0,
-                      "PARAM_FLOAT_PHASE_RESISTANCE": 0,
-                      "PARAM_FLOAT_CURRENT_MEAS_PHB": 0,
-                      "PARAM_FLOAT_CURRENT_MEAS_PHC": 0,
-                      "PARAM_FLOAT_DC_CALIB_PHB": 0,
-                      "PARAM_FLOAT_DC_CALIB_PHC": 0,
-                      "PARAM_FLOAT_SHUNT_CONDUCTANCE": 0,
-                      "PARAM_FLOAT_PHASE_CURRENT_REV_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_CURRENT_LIM": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_P_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_I_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_D": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_Q": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_IBUS": 0,
-                      "PARAM_FLOAT_ENCODER_PHASE": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_POS": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_VEL": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_KP": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_KI": 0
-                      }
+
+def hip_checker(odrive_values):
+    #checks the hip values against correct_hip_values
+    pass
 
 
-correct_knee_values = {
-                      "PARAM_FLOAT_POS_SETPOINT": 0,
-                      "PARAM_FLOAT_POS_GAIN": 0,
-                      "PARAM_FLOAT_VEL_SETPOINT": 0,
-                      "PARAM_FLOAT_VEL_GAIN": 0,
-                      "PARAM_FLOAT_VEL_INTEGRATOR_GAIN": 0,
-                      "PARAM_FLOAT_VEL_INTEGRATOR_CURRENT": 0,
-                      "PARAM_FLOAT_VEL_LIMIT": 0,
-                      "PARAM_FLOAT_CURRENT_SETPOINT": 0,
-                      "PARAM_FLOAT_CALIBRATION_CURRENT": 0,
-                      "PARAM_FLOAT_PHASE_INDUCTANCE": 0,
-                      "PARAM_FLOAT_PHASE_RESISTANCE": 0,
-                      "PARAM_FLOAT_CURRENT_MEAS_PHB": 0,
-                      "PARAM_FLOAT_CURRENT_MEAS_PHC": 0,
-                      "PARAM_FLOAT_DC_CALIB_PHB": 0,
-                      "PARAM_FLOAT_DC_CALIB_PHC": 0,
-                      "PARAM_FLOAT_SHUNT_CONDUCTANCE": 0,
-                      "PARAM_FLOAT_PHASE_CURRENT_REV_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_CURRENT_LIM": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_P_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_I_GAIN": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_D": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_Q": 0,
-                      "PARAM_FLOAT_CURRENT_CONTROL_IBUS": 0,
-                      "PARAM_FLOAT_ENCODER_PHASE": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_POS": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_VEL": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_KP": 0,
-                      "PARAM_FLOAT_ENCODER_PLL_KI": 0
-                      }
+
+def shoulder_checker(odrive_values):
+    #checks the shoulder values against correct_shoulder_values
+    pass
+
+
+
+def knee_checker(odrive_values):
+    #checks the knee values against correct_knee_values
+    pass
 
 
 
@@ -104,4 +58,17 @@ correct_knee_values = {
 def odrive_values_test(odrive_values):
     #input function
     #checks if motor is hip, shoulder, or knee and redirects appropriately
-    pass
+    
+    motor_name = list(odrive_values.keys())[0][2] #gets the third char of the motor name - will be H, S, or K
+    
+    if (motor_name == 'H'):
+        return hip_checker(odrive_values)
+    elif (motor_name == 'S'):
+        return shoulder_checker(odrive_values)
+    elif (motor_name == 'K'):
+        return knee_checker(odrive_values)
+    else:
+        return (False, {"Error": "Incorrect Motor Name"})
+
+
+odrive_values_test({"RFH": 32})
