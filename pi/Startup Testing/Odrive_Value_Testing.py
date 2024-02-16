1d510e91c6865691355f64689cdb47fbf7fa310a
ersmith01 <elaines@colostate.edu>
2024-02-15 18:20:08 -0700

    added ability to choose to not check certain values

--- pi/Startup Testing/Odrive_Value_Testing.py
+++ pi/Startup Testing/Odrive_Value_Testing.py
@@ -1,10 +1,14 @@
 #Set of functions that test if the ODESC values passed to it are correct within a certain percentage
 #Input: dict of values of the form {<motor name>: {<valuename1>: <valuenum1>.....}}
 #Output: (True, {}) if all values pass, (False, {<incorrectvals>}) if any values don't pass
+#
+#any nan values in correct_values dicts means that value is not getting checked
+
+import math
 
 # Correct Values
 correct_hip_values = {
-                      "PARAM_FLOAT_POS_SETPOINT": 0,
+                      "PARAM_FLOAT_POS_SETPOINT": float('nan'),
                       "PARAM_FLOAT_POS_GAIN": 0,
                       "PARAM_FLOAT_VEL_SETPOINT": 0,
                       "PARAM_FLOAT_VEL_GAIN": 0,
@@ -61,12 +65,14 @@ def value_checker(odrive_values, correct_values):
     
     for key, expected_value in correct_values.items():
         actual_value = odrive_value_dict[key]
+        if (math.isnan(expected_value)): #nan used for unchecked values
+            continue 
 
         if (actual_value != expected_value):
             if (not percent_error_checker(actual_value, expected_value)):
                 error_dict[key] = actual_value
 
-    return (False, error_dict)
+    return (len(error_dict) == 0, error_dict)
 
 
 
@@ -86,3 +92,35 @@ def odrive_values_test(odrive_values):
     else:
         return (False, {"Error": "odrive_values_test: incorrect motor name"})
 
+NOT_correct_hip_values = {"RFH":
+                      {"PARAM_FLOAT_POS_SETPOINT": 0,
+                      "PARAM_FLOAT_POS_GAIN": 0,
+                      "PARAM_FLOAT_VEL_SETPOINT": 0,
+                      "PARAM_FLOAT_VEL_GAIN": 0,
+                      "PARAM_FLOAT_VEL_INTEGRATOR_GAIN": 0,
+                      "PARAM_FLOAT_VEL_INTEGRATOR_CURRENT": 0,
+                      "PARAM_FLOAT_VEL_LIMIT": 0,
+                      "PARAM_FLOAT_CURRENT_SETPOINT": 0,
+                      "PARAM_FLOAT_CALIBRATION_CURRENT": 0,
+                      "PARAM_FLOAT_PHASE_INDUCTANCE": 0,
+                      "PARAM_FLOAT_PHASE_RESISTANCE": 0,
+                      "PARAM_FLOAT_CURRENT_MEAS_PHB": 0,
+                      "PARAM_FLOAT_CURRENT_MEAS_PHC": 0,
+                      "PARAM_FLOAT_DC_CALIB_PHB": 0,
+                      "PARAM_FLOAT_DC_CALIB_PHC": 0,
+                      "PARAM_FLOAT_SHUNT_CONDUCTANCE": 0,
+                      "PARAM_FLOAT_PHASE_CURRENT_REV_GAIN": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_CURRENT_LIM": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_P_GAIN": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_I_GAIN": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_D": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_V_CURRENT_CONTROL_INTEGRAL_Q": 0,
+                      "PARAM_FLOAT_CURRENT_CONTROL_IBUS": 0,
+                      "PARAM_FLOAT_ENCODER_PHASE": 0,
+                      "PARAM_FLOAT_ENCODER_PLL_POS": 0,
+                      "PARAM_FLOAT_ENCODER_PLL_VEL": 0,
+                      "PARAM_FLOAT_ENCODER_PLL_KP": 0,
+                      "PARAM_FLOAT_ENCODER_PLL_KI": 0
+                      }}
+
+print(odrive_values_test(NOT_correct_hip_values))
