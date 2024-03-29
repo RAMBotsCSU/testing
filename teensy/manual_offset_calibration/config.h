e1d6d726d076a7dd81e2ae7312c360dfe0555dff
Samuel Peccoud <samuel.peccoud@gmail.com>
2024-03-29 11:27:54 -0600

    Update offsets in config file

--- teensy/manual_offset_calibration/config.h
+++ teensy/manual_offset_calibration/config.h
@@ -1,25 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// LEFT FRONT LEG
-// float offSet40 = 0.40;  // HIP
-// float offSet51 = 0.95;  // SHOULDER
-// float offSet50 = 1.39;  // KNEE
-
-// // LEFT BACK LEG
-// float offSet41 = -0.39;  // HIP
-// float offSet61 = -0.35;  // SHOULDER
-// float offSet60 = -1.10;  // KNEE
-
-// // RIGHT FRONT LEG
-// float offSet10 = -0.47;  // HIP
-// float offSet21 = -0.64;  // SHOULDER
-// float offSet20 = -1.40;  // KNEE
-
-// // RIGHT BACK LEG
-// float offSet11 = 0.42;  // HIP
-// float offSet31 = 0.45;  // SHOULDER
-// float offSet30 = 1.65;  // KNEE
 
 // LEFT FRONT LEG
 float offSet40 = 0.40;  // HIP
@@ -28,17 +9,17 @@ float offSet50 = 1.39;  // KNEE
 
 // LEFT BACK LEG
 float offSet41 = -0.39;  // HIP
-float offSet61 = -0.35;  // SHOULDER
+float offSet61 = -0.85;  // SHOULDER
 float offSet60 = -1.10;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
 float offSet21 = -0.64;  // SHOULDER
-float offSet20 = -1.84;  // KNEE
+float offSet20 = -1.47;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
 float offSet31 = 0.45;  // SHOULDER
-float offSet30 = 1.65;  // KNEE
+float offSet30 = 1.30;  // KNEE
 
 #endif
