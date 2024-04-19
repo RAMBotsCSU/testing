4fa95a1eba248895a34da946b2f9929e56f1dec8
Jacob Beauchamp <74421153+BeauchampJ@users.noreply.github.com>
2024-04-19 07:19:53 -0600

    Update config.h
    Updated leg tunes for E-days

--- teensy/manual_offset_calibration/config.h
+++ teensy/manual_offset_calibration/config.h
@@ -4,22 +4,20 @@
 
 // LEFT FRONT LEG
 float offSet40 = 0.40;  // HIP
-float offSet51 = 0.95;  // SHOULDER
-float offSet50 = 1.39;  // KNEE
-
+float offSet51 = 0.75;  // SHOULDER
+float offSet50 = 0.89;  // KNEE
 // LEFT BACK LEG
 float offSet41 = -0.39;  // HIP
-float offSet61 = -0.85;  // SHOULDER
-float offSet60 = -1.10;  // KNEE
+float offSet61 = -0.65;  // SHOULDER
+float offSet60 = -1.05;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
-float offSet21 = -0.64;  // SHOULDER
-float offSet20 = -1.47;  // KNEE
+float offSet21 = -0.44;  // SHOULDER
+float offSet20 = -0.90;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
-float offSet31 = 0.45;  // SHOULDER
-float offSet30 = 1.30;  // KNEE
-
+float offSet31 = 0.25;  // SHOULDER
+float offSet30 = 0.85;  // KNEE
 #endif
